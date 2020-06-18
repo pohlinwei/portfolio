383b2e76ccc49205db7292fd70c143dab221bcc3
Lin Wei <linwei0909@hotmail.com>
2020-06-18 22:42:51 +0800

    Remove unrelated code

--- script.js
+++ script.js
@@ -133,30 +133,3 @@ const setupActionAnimation = () => {
 }
 
 setupActionAnimation();
-
-/**
- * Fetches response from the server and displays it under 'Contact' section.
- */
-const fetchAndShowResponse = () => {
-  const commentPlaceholder = document.getElementById('comment-placeholder');
-  try {
-    ensureNonNull(commentPlaceholder);
-  } catch (err) {
-    console.log(error);
-  }
-  fetch('/submit_comment').then(response => response.json()).then(comments => {
-    comments.forEach(commentText => {
-      commentPlaceholder.appendChild(createCommentElement(commentText))
-    });
-  });
-}
-
-/** Creates an element which contains the comment text. */
-const createCommentElement = (commentText) => {
-  const comment = document.createElement('li');
-  comment.innerHTML = commentText;
-  return comment;
-}
-
-const body = document.getElementsByTagName('body')[0];
-// body.onload = fetchAndShowResponse;
