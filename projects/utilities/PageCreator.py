efdb528967b450baa883eb5c8edd6f09a06d63a4
Lin Wei <linwei0909@hotmail.com>
2020-06-19 21:26:25 +0800

    Remove unnecessary print statements

--- projects/utilities/PageCreator.py
+++ projects/utilities/PageCreator.py
@@ -37,7 +37,6 @@ class PageCreator:
 
     def get_screenshots_html(self, screenshots_path, name):
         screenshots_dir = Path(screenshots_path)
-        print(listdir(screenshots_dir))
         screenshots = [screenshots_path + '/' + screenshot for screenshot in listdir(screenshots_dir)
                         if screenshot.endswith(('.jpg', '.png'))]
         screenshot_pairs = ScreenshotPair.get_pairs(screenshots)
