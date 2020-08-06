6270559c13fd17918fa62214092a47c3237a30db
Lin Wei <linwei0909@hotmail.com>
2020-08-06 22:17:39 +0800

    Update scripts for generating pages

---
+++ projects/utilities/PageCreator.py
@@ -0,0 +1,70 @@
+from .projectutilities import create_file
+from os import listdir
+from pathlib import Path
+from .Project import Project
+from .ScreenshotPair import ScreenshotPair
+
+class PageCreator:
+    # assumes that relative paths are correct
+    PARENT_PARENT_DIR_NAME = 'projects'
+    # templates for page creation
+    BUTTON_TEMPLATE = Path('templates/button_template.html')
+    IMG_TEMPLATE = Path('templates/image_template.html')
+    DESCRIPTION_TEMPLATE = Path('templates/description_template.html')
+    MAIN_TEMPLATE = Path('templates/project_template.html')
+
+    def create(self, project, overwrite):
+        proj_page_path = project.get_page()
+        relative_page_path = self.to_relative_path_from_ancestor(proj_page_path)
+        page = Path(relative_page_path)
+
+        if page.exists() and not overwrite:
+            print('Skipping the creation of .html page at ' + proj_page_path)
+            print('To force the creation of this file, set \'overwrite\' to true')
+            return
+        elif not page.exists():
+            create_file(relative_page_path)
+        
+        page_content = self.generate_content(project)
+        page.write_text(page_content)
+
+    def generate_content(self, project):
+        name = project.get_name() 
+        description = self.get_description_html(project.get_description())
+        images = self.get_screenshots_html(project.get_screenshots(), name)
+        github_button = self.get_github_button_html(project.get_github())
+
+        main_template = PageCreator.MAIN_TEMPLATE.read_text()
+        return main_template.format(NAME=name, DESCRIPTION=description, GITHUB_BUTTON=github_button, IMAGES=images)
+
+    def get_screenshots_html(self, screenshots_path, name):
+        screenshots_dir = Path(screenshots_path)
+        screenshots = [screenshots_path + '/' + screenshot for screenshot in listdir(screenshots_dir)
+                        if screenshot.endswith(('.jpg', '.png'))]
+        screenshot_pairs = ScreenshotPair.get_pairs(screenshots)
+
+        image_template = PageCreator.IMG_TEMPLATE.read_text()
+        images = []
+        images = [image_template.format(SHOTS_SMALL=screenshot_pair.get_small(), 
+                    SHOTS=screenshot_pair.get_reg(), NAME=name) for screenshot_pair in screenshot_pairs]
+        
+        return ''.join([image for image in images])
+    
+    def get_description_html(self, description):
+        description_template = PageCreator.DESCRIPTION_TEMPLATE.read_text()
+        return description_template.format(DESCRIPTION=description)
+
+    def get_github_button_html(self, github):
+        if github == '':
+            return ''
+        github_button_template = PageCreator.BUTTON_TEMPLATE.read_text()
+        return github_button_template.format(GITHUB=github)
+    
+    def to_relative_path_from_ancestor(self, path):
+        dirs = path.split('/')
+        parent_dir = dirs[0]
+
+        if parent_dir != PageCreator.PARENT_PARENT_DIR_NAME:
+            raise ValueError('Incompatible relative paths when converting to relative path')
+
+        return '/'.join(dirs[1:])
---
+++ projects/utilities/projectutilities.py
@@ -0,0 +1,42 @@
+from errno import EEXIST
+from os import strerror
+from pathlib import Path
+
+# all prefixes that are used to define a project
+PREFIX_FORMAT = '###{type}###'
+NAME_PREFIX = PREFIX_FORMAT.format(type='NAME')
+IMG_PREFIX = PREFIX_FORMAT.format(type='IMAGE')
+SUMMARY_PREFIX = PREFIX_FORMAT.format(type='SUMMARY')
+DESCRIPTION_PREFIX = PREFIX_FORMAT.format(type='DESCRIPTION')
+TOOLS_PREFIX = PREFIX_FORMAT.format(type='TOOLS')
+DATE_PREFIX = PREFIX_FORMAT.format(type='DATE')
+PAGE_PREFIX = PREFIX_FORMAT.format(type='PAGE')
+SCREENSHOTS_PREFIX = PREFIX_FORMAT.format(type='SCREENSHOTS')
+GITHUB_PREFIX = PREFIX_FORMAT.format(type='GITHUB')
+PREFIXES = [NAME_PREFIX, IMG_PREFIX, SUMMARY_PREFIX, DESCRIPTION_PREFIX, TOOLS_PREFIX, DATE_PREFIX, PAGE_PREFIX,
+            SCREENSHOTS_PREFIX, GITHUB_PREFIX]
+
+def create_template(file_path):
+    p = Path(file_path)
+
+    if p.exists():
+        raise FileExistsError(EEXIST, strerror(EEXIST), 
+                                'File already exists at {}'.format(file_path))
+    
+    p.write_text('\n'.join(PREFIXES))
+
+def create_file(file_path):
+    dirs = file_path.split('/')[:-1]
+    subdirs_path = '/'.join(dirs)
+    # create intermediate dirs (if necessary)
+    create_dir(subdirs_path)
+    # create file
+    Path(file_path).touch()
+
+def create_dir(dir_path):
+    directory = Path(dir_path)
+    if not directory.exists():
+        directory.mkdir(parents=True)
+
+def remove_file_ext(file_path):
+    return ''.join(file_path.split('.')[:-1])
