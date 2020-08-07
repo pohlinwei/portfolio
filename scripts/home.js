25f3fbc1d914e8ab49c49364e3492326d6ad10cf
Lin Wei <linwei0909@hotmail.com>
2020-08-07 23:01:08 +0800

    Add dropdown menu

--- scripts/home.js
+++ scripts/home.js
@@ -16,3 +16,32 @@ const setupActionAnimation = () => {
     };
     window.setInterval(actionsAnimate, 2000);
 };
+const setupDropdownMenu = () => {
+    const menuButton = document.getElementById('menu-button');
+    ensureNonNull(menuButton);
+    menuButton.onclick = () => toggleMain(false);
+    const closeButton = document.getElementById('close-button');
+    ensureNonNull(closeButton);
+    closeButton.onclick = () => toggleMain(true);
+    const menuList = document.getElementById('menu-list');
+    ensureNonNull(menuList);
+    const menuItems = menuList.children;
+    for (let i = 0; i < menuItems.length; i++) {
+        menuItems[i].onclick = () => toggleMain(true);
+    }
+};
+const toggleMain = (shouldShowMain) => {
+    const mainDisplayValue = shouldShowMain ? Display.SHOW : Display.HIDE;
+    const menuDisplayValue = shouldShowMain ? Display.HIDE : Display.SHOW;
+    const sections = document.getElementsByTagName('section');
+    ensureNonNull(sections);
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].style.display = mainDisplayValue;
+    }
+    const footer = document.querySelector('footer');
+    ensureNonNull(footer);
+    footer.style.display = mainDisplayValue;
+    const menu = document.getElementById('menu');
+    ensureNonNull(menu);
+    menu.style.display = menuDisplayValue;
+};
--- scripts/home.ts
+++ scripts/home.ts
@@ -19,3 +19,40 @@ const setupActionAnimation = () => {
   
   window.setInterval(actionsAnimate, 2000);
 }
+
+const setupDropdownMenu = () => {
+  const menuButton = <HTMLDivElement> document.getElementById('menu-button');
+  ensureNonNull(menuButton);
+  menuButton.onclick = () => toggleMain(false);
+
+  const closeButton = <HTMLDivElement> document.getElementById('close-button');
+  ensureNonNull(closeButton);
+  closeButton.onclick = () => toggleMain(true);
+
+
+  const menuList = <HTMLElement> document.getElementById('menu-list');
+  ensureNonNull(menuList);
+  const menuItems = <HTMLCollectionOf<HTMLElement>> menuList.children;
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].onclick = () => toggleMain(true);
+  }
+
+  const sections = <HTMLCollectionOf<HTMLElement>> document.getElementsByTagName('section');
+  ensureNonNull(sections);
+  const footer = <HTMLElement> document.querySelector('footer');
+  ensureNonNull(footer);
+  const menu = <HTMLDivElement> document.getElementById('menu');
+  ensureNonNull(menu);
+  
+  const toggleMain = (shouldShowMain: boolean) => {
+    const mainDisplayValue = shouldShowMain ? Display.SHOW : Display.HIDE;
+    const menuDisplayValue = shouldShowMain ? Display.HIDE : Display.SHOW;
+  
+    for (let i = 0; i < sections.length; i++) {
+      sections[i].style.display = mainDisplayValue;
+    }
+
+    footer.style.display = mainDisplayValue;
+    menu.style.display = menuDisplayValue;
+  }
+}
